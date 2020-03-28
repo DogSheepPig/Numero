806da588bf7b305fd933e3cdc5855418ed2b9296
Richard Jang <rjang@dogsheep.com>
2020-03-28 02:04:09 -0700

    Implement convert

---
+++ NumeroTests/ConverterTests.swift
@@ -0,0 +1,29 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by Richard Jang on 2020-03-28.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  var converter: Converter!
+
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+}
